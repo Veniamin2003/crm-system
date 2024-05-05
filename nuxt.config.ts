5491983e64c4afde3566eca873d6392132a973ad
Veniamin2003 <veniapetrov941@gmail.com>
2024-05-05 23:05:46 +0500

    установка зависимостей

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // @ts-ignore
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss']
+  modules: [
+      '@nuxt/image',
+      '@nuxtjs/tailwindcss',
+      'shadcn-nuxt',
+      [
+          '@nuxtjs/google-fonts',
+        {
+          families: {
+            Lato: {
+                wght: [300, 499, 700],
+                ital: [300],
+            },
+          },
+        },
+      ],
+  ],
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui',
+  },
 })
